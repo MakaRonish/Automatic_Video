0aabe50ced66226d71363ca578dbf15c5441ffc2
MakaRonish <ronishmakaju12345@gmail.com>
2025-03-12 23:56:37 -0400

    automaticly types and downloads

--- main.py
+++ main.py
@@ -1,6 +1,21 @@
 import webbrowser
+import pyautogui
+import time
 
 url = "https://reelit.net/#google_vignette"
+insta_link = "https://www.instagram.com/reel/DHHdXmNikbg/?utm_source=ig_web_copy_link&igsh=MzRlODBiNWFlZA=="
 
 
 webbrowser.open_new(url)
+time.sleep(3)
+
+pyautogui.scroll(-600)
+time.sleep(3)
+pyautogui.click(829, 867)
+pyautogui.typewrite(insta_link)
+pyautogui.press("enter")
+pyautogui.scroll(-200)
+time.sleep(2)
+pyautogui.click(913, 1447)
+
+print(pyautogui.position())
--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,1 +1,13 @@
-webbrowser.open_new(url)
+url = "https://reelit.net/#google_vignette"
+# insta_link = "https://www.instagram.com/reel/DHHdXmNikbg/?utm_source=ig_web_copy_link&igsh=MzRlODBiNWFlZA=="
+
+
+# webbrowser.open_new(url)
+# time.sleep(3)
+
+# pyautogui.scroll(-600)
+# time.sleep(3)
+# pyautogui.click(829, 867)
+# pyautogui.typewrite(insta_link)
+# pyautogui.press("enter")
+# pyautogui.scroll(-200)
